1f4c0da678de4d337b4c8e47db9b3b536e9b1ad9
BrianAnderson <banderson@dev-10.com>
2023-07-26 15:48:18 -0400

    add db schema work

--- server/db/myChessJourneyData.sql
+++ server/db/myChessJourneyData.sql
@@ -1,29 +1,64 @@
--- drop tables if they exist 
+-- drop database if exists
+DROP DATABASE IF EXISTS mychessjourney;
+CREATE DATABASE mychessjourney;
+use mychessjourney;
 
-DROP TABLE IF EXISTS game
-DROP TABLE IF EXISTS user 
-DROP TABLE IF EXISTS post 
+
+
+
+-- drop tables if they exist
+
+DROP TABLE IF EXISTS game;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
 
 -- now create the tables 
 
 CREATE TABLE game(
-    'gameid' INT NOT NULL AUTO_INCREMENT, 
-    'playerone' VARCHAR(255), 
-    'playertwo' VARCHAR(255),
-    PRIMARY KEY ('gameid')
+    gameid INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    playerone VARCHAR(255),
+    playertwo VARCHAR(255)
 );
 
-CREATE TABLE user(
-    'userid' INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY ('userid')
+CREATE TABLE appuser(
+    userid INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    passwordhash VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE role(
+    approleid INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE userrole(
+    userid INT NOT NULL,
+    roleid INT NOT NULL,
+    CONSTRAINT pkappuserrole
+        PRIMARY KEY (userid, roleid),
+    CONSTRAINT fkuserid
+        FOREIGN KEY (userid)
+        REFERENCES appuser(userid),
+    CONSTRAINT fkroleid
+        FOREIGN KEY (roleid)
+        REFERENCES role(approleid)
 );
 
 CREATE TABLE post(
-    'postid' INT NOT NULL AUTO_INCREMENT, 
-    'title' VARCHAR(255), 
-    'created' DATETIME, 
-    'content' TEXT
+    postid INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    title VARCHAR(255),
+    created DATETIME,
+    content TEXT
 );
+
+
+-- insert into the db what we need to function
+
+INSERT INTO role (`name`) VALUES
+    ('USER'),
+    ('ADMIN');
+
+
 
 
 
